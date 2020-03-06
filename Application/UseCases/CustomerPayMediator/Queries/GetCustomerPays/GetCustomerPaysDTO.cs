78ef021576914a1a565aea756e85cb96044c288e
Mia <miahljnh@gigaming.io>
2020-03-06 10:35:38 +0700

    .

--- Application/UseCases/CustomerPayMediator/Queries/GetCustomerPays/GetCustomerPaysDTO.cs
+++ Application/UseCases/CustomerPayMediator/Queries/GetCustomerPays/GetCustomerPaysDTO.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using Task18_BootcampRefactory.Application.UseCases.CustomerPayMediator.Queries.GetCustomerPay;
 using Task18_BootcampRefactory.Model;
 
 namespace Task18_BootcampRefactory.Application.UseCases.CustomerPayMediator.Queries.GetCustomerPays
 {
     public class GetCustomerPaysDTO : BaseDTO
     {
-        public List<Customers_Payment_Card> Data { get; set; }
+        public List<CustomerPayData> Data { get; set; }
     }
 }
--- Application/UseCases/CustomerPayMediator/Request/CustomerPayDTO.cs
+++ Application/UseCases/CustomerPayMediator/Request/CustomerPayDTO.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using Task18_BootcampRefactory.Application.UseCases.CustomerPayMediator.Queries.GetCustomerPay;
 using Task18_BootcampRefactory.Model;
 
 namespace Task18_BootcampRefactory.Application.UseCases.CustomerPayMediator.Request
 {
     public class CustomerPayDTO : BaseDTO
     {
-        
+        public List<CustomerPayData> Data { get; set; }
     }
 }
